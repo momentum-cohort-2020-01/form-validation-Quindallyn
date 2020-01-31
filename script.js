b8851e120f7cfcb295bbcb2f6c18b17135e33f47
Quindallyn <qrk5002@gmail.com>
2020-01-31 16:23:48 -0500

    invalid and valid inputs

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Add validation!');
+let boxes = document.querySelectorAll('.box')
+console.log(boxes)
+
+let form = document.querySelector("#parking-form")
+
+form.addEventListener ("submit", function(event){
+    event.preventDefault()
+    for (let box of boxes){
+        console.log (box.value)
+        if (box.value === ''){
+            console.log("invalid")
+            box.parentElement.classList.add("input-invalid")
+        }
+        else{
+            console.log("valid")
+            box.parentElement.classList.add("input-valid")
+        }
+    // let button = document.querySelector (".box")   
+    }  
+})
